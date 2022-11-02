c1e53f34b26bff80a54696821a51b21770701d69
Fredy Laksmono <fredy.laksmono@gmail.com>
2022-11-01 20:29:00 -0500

    Clean up seed file

--- seed/amusementParkSeed.js
+++ seed/amusementParkSeed.js
@@ -1,49 +1,38 @@
-const db = require('../db')
-const Ride = require('../models/ride')
-const User = require('../models/user')
-const Review = require('../models/review')
+const db = require("../db");
+const Ride = require("../models/ride");
+const User = require("../models/user");
+const Review = require("../models/review");
 
-db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 const main = async () => {
-
   const rides = [
     {
-      name: 'Roller Coaster',
-      description:
-        'This is about the roller coaster.',
+      name: "Roller Coaster",
+      description: "This is about the roller coaster.",
       image:
-        'https://upload.wikimedia.org/wikipedia/commons/4/4b/Aloe_vera_flower_inset.png',
+        "https://upload.wikimedia.org/wikipedia/commons/4/4b/Aloe_vera_flower_inset.png",
       location: "theme park",
-      heightRequirement: '4',
+      heightRequirement: "4",
       reviewId: "636025e88310aa9a05431557"
-      
     }
-  ]
+  ];
 
-  const user = [
+  const review = [
     {
-      id: '12345',
-      userName: 'test'
+      date: "2022-10-31",
+      rideId: "636025e88310aa9a05431539",
+      comment: "This is the best ride ever!!!"
     }
-  ]
+  ];
 
-  const review=[{
-    
-        date: "2022-10-31",
-        rideId: "636025e88310aa9a05431539",
-        comment: "This is the best ride ever!!!"
-    
-  }]
-
-  await Ride.insertMany(rides)
-  await User.insertMany(user)
-  await Review.insertMany(review)
-  console.log('Created some rides!')
-}
+  await Ride.insertMany(rides);
+  await Review.insertMany(review);
+  console.log("Created some rides!");
+};
 const run = async () => {
-  await main()
-  db.close()
-}
+  await main();
+  db.close();
+};
 
-run()
+run();
